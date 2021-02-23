1c1d99456a3412a74cf494a93469532488db4459
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-22 21:00:47 -0600

    imported and added browser router to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Amelia from './components/Amelia/Amelia'
 
 function App() {
   return (
     <div className="wrapper">
-      <h1>
-        AKJ
-      </h1>
-      <Amelia />
+      <h1>AKJ</h1>
+      <BrowserRouter>
+        <Amelia />
+      </BrowserRouter>
     </div>
   );
 }
