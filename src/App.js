7b3b7bd8f017973408097c97664d7dfb403ba031
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-23 16:09:41 -0600

    import Link from react in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Amelia from './components/Amelia/Amelia'
 import Karoline from './components/Karoline/Karoline'
