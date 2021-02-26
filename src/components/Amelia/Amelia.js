9132984eeb24e2fefdcac434437ef534460af469
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-26 12:14:37 -0600

    search parameters added to Amelia.js

--- src/components/Amelia/Amelia.js
+++ src/components/Amelia/Amelia.js
@@ -4,9 +4,14 @@ import Beluga from './Beluga';
 import Blue from './Blue';
 
 export default function Amelia() {
+  const { search } = useLocation();
+  const match = search.match(/type=(.*)/);
+  const type = match?.[1];
   return (
     <div className="aWrapper">
       <h2>Amelia<span>!</span></h2>
+      {type === 'beluga' && <Beluga />}
+      {type === 'blue' && <Blue />}
     </div>
   );
 }
