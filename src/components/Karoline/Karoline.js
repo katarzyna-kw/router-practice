bdbc0724614c60d2dc8443ea27e649cffbc712dc
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-27 10:54:01 -0600

    useRouteMatch added to function Karoline

--- src/components/Karoline/Karoline.js
+++ src/components/Karoline/Karoline.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams, useRouteMatch } from 'react-router-dom';
 
 export default function Karoline() {
+  const { path } = useRouteMatch();
 
   return <h2>Karoline</h2>;
 }
